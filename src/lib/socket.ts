4bc682ce0eed21fc62479f3dd024930273d0183f
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2024-08-17 01:11:26 +0530

    minor fix

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,6 +1,6 @@
 // Client code (socket.ts)
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8000";
+const URL = process.env.VITE_BACKEND_URL || "http://localhost:8000";
 
 export const socket = io(URL);
